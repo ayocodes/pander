39686d2bd0c35faf56eb8b58804e671e46bb707d
Ayo <omotolatomi01@gmail.com>
2025-05-03 20:51:18 +0100

    chore: remove poll monitoring and job queue management files

--- app/website/library/queue/workers.ts
+++
@@ -1,5 +0,0 @@
-import { pollMonitorWorker } from "./poll-monitor-worker.js";
-import { epochDistributionWorker } from "./epoch-distribution-worker.js";
-import { pollResolutionWorker } from "./poll-resolution-worker.js";
-
-export { pollMonitorWorker, epochDistributionWorker, pollResolutionWorker };
