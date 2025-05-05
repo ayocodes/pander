ebe47fdbfc680295392e72c43599800c93fc520f
Ayo <omotolatomi01@gmail.com>
2025-05-05 02:06:13 +0100

    refactor: simplify Ponder configuration by consolidating RPC URL handling and adjusting transport settings for Pharos Devnet

--- app/indexer/ponder.config.ts
+++ app/indexer/ponder.config.ts
@@ -7,54 +7,13 @@ dotenv.config();
 import { CapyCoreAbi, CapyPollAbi } from "./abis/pandpoll-abi";
 import { capyCoreAddress, startBlock, network } from "./constants";
 
-// Create an array of transports based on available environment variables
-const transports = [];
-
-if (process.env.PONDER_RPC_URL_1) {
-  transports.push(
-    rateLimit(http(process.env.PONDER_RPC_URL_1), {
-      requestsPerSecond: Number(process.env.PONDER_REQUESTS_PER_SECOND_1 || 50),
-    })
-  );
-}
-
-if (process.env.PONDER_RPC_URL_2) {
-  transports.push(
-    rateLimit(http(process.env.PONDER_RPC_URL_2), {
-      requestsPerSecond: Number(process.env.PONDER_REQUESTS_PER_SECOND_2 || 50),
-    })
-  );
-}
-
-if (process.env.PONDER_RPC_URL_3) {
-  transports.push(
-    rateLimit(http(process.env.PONDER_RPC_URL_3), {
-      requestsPerSecond: Number(process.env.PONDER_REQUESTS_PER_SECOND_3 || 50),
-    })
-  );
-}
-
-if (process.env.PONDER_RPC_URL_4) {
-  transports.push(
-    rateLimit(http(process.env.PONDER_RPC_URL_4), {
-      requestsPerSecond: Number(process.env.PONDER_REQUESTS_PER_SECOND_4 || 50),
-    })
-  );
-}
-
-if (process.env.PONDER_RPC_WS_URL) {
-  transports.push(
-    rateLimit(webSocket(process.env.PONDER_RPC_WS_URL), {
-      requestsPerSecond: Number(process.env.PONDER_REQUESTS_PER_SECOND_5 || 50),
-    })
-  );
-}
-
 export default createConfig({
   networks: {
     pharosDevnet: {
       chainId: 50002,
-      transport: http(process.env.PONDER_RPC_URL_4), //loadBalance(transports),
+      transport: http(process.env.PONDER_RPC_URL), //loadBalance(transports),
+      pollingInterval: 12000,
+      maxRequestsPerSecond: 2
     },
     anvil: {
       chainId: 31337,
